e4644ccc21542936a95374a177ac354e212f3769
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-08-05 17:20:32 +0200

    React component by function created

---
+++ 02/app.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// ----------------------- Function ----------------------------
+
+const HeaderFn = () => <header>Moja pierwsza strona w React</header>
+
+ReactDOM.render(
+
+    <HeaderFn/>,
+    document.querySelector('#root')
+
+)
+
+// -------------------- End of Function ------------------------
+
+// ----------------------- ES2015 ------------------------------
+
+
+
+// -------------------- End of ES2015 -------------------------
